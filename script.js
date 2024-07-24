7544dc61bb268582e952d084683433b52238093a
Rylan Ho <170477157+rylanh1234@users.noreply.github.com>
2024-07-24 16:36:52 -0600

    Moved action related to the intial grid creation to a function
    New grid created upon clicking button by removing
    previous grid and then using mentioned
    function
    The first new creation works fine, but subsequent ones
    have width right but not height(number of rows)

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 const container = document.querySelector("#container")
 
 let numberDiv = 16;
-for (let i = 0; i < numberDiv**2; i++) {
-const griddiv = document.createElement("div");
-griddiv.classList.add("griddiv");
-container.appendChild(griddiv);
-}
+function createGrid(numberDiv) {
+    for (let i = 0; i < numberDiv**2; i++) {
+        const griddiv = document.createElement("div");
+        griddiv.classList.add("griddiv");
+        container.appendChild(griddiv);
+    }
+    document.documentElement.style.setProperty('--rowWidth',numberDiv); // defines a custom css variable as "numberDiv"
 
-const gridarray = document.querySelectorAll(".griddiv")
-gridarray.forEach((griddiv) => {
-    griddiv.addEventListener("mouseenter", function(e) {
-        e.target.style = "background:blue"
+    const gridarray = document.querySelectorAll(".griddiv")
+    gridarray.forEach((griddiv) => {
+        griddiv.addEventListener("mouseenter", function(e) {
+            e.target.style = "background:blue"
+        })
     })
-})
+}
+createGrid(numberDiv);
 
 const sizebutton = document.querySelector("button")
 const children = container.querySelectorAll("*");
@@ -21,11 +25,7 @@ sizebutton.addEventListener("click", ()=> {
     children.forEach((griddiv) => {
         griddiv.remove()
     })
-    for (let i = 0; i < numberDiv**2; i++) {
-        const griddiv = document.createElement("div");
-        griddiv.classList.add("griddiv");
-        container.appendChild(griddiv);
-        }
+    createGrid(numberDiv)
 })
 
-document.documentElement.style.setProperty('--rowWidth',numberDiv); // defines a custom css variable as "numberDiv"
+
