6ab039c339c08d244ea714e4e7b5bd20c9296e6d
Rylan Ho <170477157+rylanh1234@users.noreply.github.com>
2024-07-24 17:38:52 -0600

    Randomizes the squares' RGB values with each interaction     uses   `` for interpolation (imbed expression     inside string using ${})
        only changes the squares' colour if its background is ""
        (default/white)

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+function randInt() {
+    return Math.floor(Math.random()*256);
+}
+
 const container = document.querySelector("#container")
 
 let numberDiv = 16;
+let r = 0;
+let g = 0;
+let b = 0;
 function createGrid(numberDiv) {
     for (let i = 0; i < numberDiv**2; i++) {
         const griddiv = document.createElement("div");
@@ -12,7 +19,12 @@ function createGrid(numberDiv) {
     const gridarray = document.querySelectorAll(".griddiv")
     gridarray.forEach((griddiv) => {
         griddiv.addEventListener("mouseenter", function(e) {
-            e.target.style = "background:blue"
+            if (e.target.style.background === "") {
+                r = randInt();
+                g = randInt();
+                b = randInt();
+                e.target.style.background = `rgb(${r}, ${g}, ${b})`;
+            }
         })
     })
 }
